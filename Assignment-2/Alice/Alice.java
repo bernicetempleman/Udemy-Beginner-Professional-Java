5526a4e1879d9c87348e4665043e125dbae102e2
Bernice Templeman <bernice.templeman@gmail.com>
2016-01-07 19:34:49 -0500

    Add Assignment-2

---
+++ Assignment-2/Alice/Alice.java
@@ -0,0 +1,31 @@
+import static java.lang.System.out;
+
+/**
+ * Description of Alice:
+ * Class: Udemy "Beginner Professional Java"
+ * Assignment 2: Story Time
+ * Printing text
+ * 
+ * @author Bernice Templeman 
+ * @version 20160107
+ */
+public class Alice 
+{
+    public static void main(String[] args)
+    {
+        out.println("CHAPTER I. Down the Rabbit-Hole\n");
+        out.println("Alice was beginning to get very tired of sitting by her sister");
+        out.println("on the bank, and of having nothing to do: once or twice she");
+        out.println("had peeped into the book her sister was reading, but it had");
+        out.println("no pictures or conversations in it, \"and what is the use of");
+        out.println("a book,\" thought Alice \"without pictures or conversations?\"\n");
+        out.println("So she was considering in her own mind (as well as she could,");
+        out.println("for the hot day made her feel very sleepy and stupid), whether");
+        out.println("the pleasure of making a daisy-chain would be worth the trouble");
+        out.println("of getting up and picking the daisies, when suddenly a White");
+        out.println("Rabbit with pink eyes ran close by her.");
+        
+      
+    }
+
+}
