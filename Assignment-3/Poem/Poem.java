d8e0be7e29ff39e0c8a9f00d9df8b93bb90d9edc
Bernice Templeman <bernice.templeman@gmail.com>
2016-01-07 21:46:50 -0500

    Add Assignment-3

---
+++ Assignment-3/Poem/Poem.java
@@ -0,0 +1,61 @@
+import static java.lang.System.out;
+
+
+/**
+ * Write a description of Poem here.
+ * 
+ * Your program will print the following text 
+ * (a snippet from Through the Looking Glass, 
+ * which is now public domain as its copyright has expired). 
+ * Pay careful attention to spacing and newlines.
+ * Compile and run your code. Check your results carefully.
+ * 
+ * @author Bernice Templeman
+ * @version 20160107
+ */
+public class Poem 
+{
+    public static void main(String[] args)
+    {
+        out.println("JABBERWOCKY\n");
+        out.println("'Twas brillig, and the slithy toves");
+        out.println("   Did gyre and gimble in the wabe;");
+        out.println("All mimsy were the borogoves,");
+        out.println("   And the mome raths outgrabe.\n");
+        out.println("\"Beware the Jabberwock, my son!");
+        out.println("   The jaws that bite, the claws that catch!");
+        out.println("Beware the Jubjub bird, and shun");
+        out.println("   The frumious Bandersnatch!\"\n");
+        out.println("He took his vorpal sword in hand:");
+        out.println("   Long time the manxome foe he sought - ");
+        out.println("So rested he by the Tumtum tree,");
+        out.println("   And stood awhile in thought.\n");
+        out.println("And as in uffish thought he stood,");
+        out.println("   The Jabberwock, with eyes of flame,");
+        out.println("Came whiffling through the tulgey wood,");
+        out.println("   And burbled as it came!\n");
+        out.println("One, two! One, two! And through and through");
+        out.println("   The vorpal blade went snicker-snack!");
+        out.println("He left it dead, and with its head");
+        out.println("   He went galumphing back.\n");
+        out.println("\"And hast thou slain the Jabberwock?");
+        out.println("   Come to my arms, my beamish boy!");
+        out.println("O frabjous day! Callooh! Callay!\"");
+        out.println("   He chortled in his joy.\n");
+        out.println("'Twas brillg, and the slithy toves");
+        out.println("   Did gyre and gimble in the wabe;");
+        out.println("All mimsy were the borogoves,");
+        out.println("   And the mome raths outgrabe.\n"); 
+        out.println("\"It seems very pretty,\" she said when she had finished");
+        out.println("it, \"but it's RATHER hard to understand!\" (You see");
+        out.println("she didn't like to confess, ever to herself, that ");
+        out.println("she couldn't make it out at all.) \"Somehow it seems");
+        out.println("to fill my head with ideas - only I don't exactly");
+        out.println("know what they are! However, SOMEBODY killed");
+        out.println("SOMETHING: that's clear, at any rate.\"");
+        
+        
+        
+    }
+
+}
