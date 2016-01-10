b252260286d907461361747aa59ce8776a274738
Bernice Templeman <bernice.templeman@gmail.com>
2016-01-10 16:29:05 -0500

    Add Assignment-9

---
+++ Assignment-9/Whatever/Whatever.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of Whatever here.
+ * 
+ * @author Bernice Templeman
+ * @version 20160110
+ */
+public class Whatever {
+    public static void theFunctionName()      
+    {        
+        // do stuff here 
+        System.out.println("With a knick-knack paddywhack, \nGive the dog a bone, \nThis old man came rolling home.\n");
+    }   
+
+    public static void main(String[] args)      
+    {         
+        // call a function  
+        System.out.println("This old man, he played one. \nHe played knick-knack on my thumb;");   
+        theFunctionName(); 
+        
+        System.out.println("This old man, he played two. \nHe played knick-knack on my shoe;");
+        theFunctionName(); 
+        
+        System.out.println("This old man, he played three. \nHe played knick-knack on my knee;");
+        theFunctionName(); 
+        
+        System.out.println("This old man, he played four. \nHe played knick-knack on my door;"); 
+        theFunctionName();
+        
+        System.out.println("This old man, he played five. \nHe played knick-knack on my hive;");
+        theFunctionName();
+        
+        System.out.println("This old man, he played six. \nHe played knick-knack on my sticks;");
+        theFunctionName();
+        
+    }
+}
