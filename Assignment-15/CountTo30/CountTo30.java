97d16aa852bb04b0c0770f7af986e3db168c3ffe
Bernice Templeman <bernice.templeman@gmail.com>
2016-01-12 19:55:16 -0500

    Add Assignement-15

---
+++ Assignment-15/CountTo30/CountTo30.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of CountTo30 here.
+ * 
+ * Learning Goals
+ * Understand how to apply while loops to a wider variety of patterns.  
+ * 
+ * Business Goal 
+ * Print the numbers from 1 to 30 by using a while loop, 
+ * and as few lines of code as possible.  
+ * This is a quickie practice problem leading into upcoming concepts.  
+ * 
+ * Planning Your Code 
+ * 1. Create one variable to hold the number that is counting up. 
+ * 2. Write the code. 
+ * 3. Run the code and work out the bugs. 
+ * 4. See if you can find anywhere to shorten the code.  
+ * 
+ * @author Bernice 
+ * @version 20160112
+ */
+public class CountTo30 {
+
+    public static void main(String[] args){
+
+        // do stuff before  
+        int number  = 1;
+        
+        while (number <= 30)  {       
+            // do stuff in loop 
+            System.out.println(number);
+            number++;
+            
+        } 
+
+    }
+
+}
