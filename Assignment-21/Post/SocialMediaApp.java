3b98ff0216a1c06d29a5ad2f6f44141cbc9cd3af
Bernice Templeman <bernice.templeman@gmail.com>
2016-01-24 22:06:00 -0500

    Add updates to a simple menu for part 2

--- Assignment-21/Post/SocialMediaApp.java
+++ Assignment-21/Post/SocialMediaApp.java
@@ -13,18 +13,22 @@ import java.util.Scanner;
  * You are currently user "pug". What would you like to do?
  * 
  * Behaviors should be fairly apparent from the names.
+ * 
  * If the user asks to create a new user, you ask for the fields that make up the user 
  * (typed in from the keyboard) and create a new user instance from that.
  * You'll need to keep a list of users in an array. 
  * It's okay to make an array bigger than you expect to use,
  * so you don't need to use ArrayList yet (unless you want to).
  * Once they finish entering the user, go back to the main menu.
+ * 
  * If the user asks to become an existing user, print a list of their names, 
  * and let the user pick which one to become. 
  * You can do this by number or by name, your choice. Then return to the main menu.
+ * 
  * If the user asks to create a post, find the last post made by that user, and print it to the screen. 
  * Then ask the user to type in the next post (including the date, typed in).
  * When they're done entering it, and you've saved it in the posts array, go back to the main menu.
+ * 
  * If the user asks to print all posts, go through the post array and print all the fields of all the posts.
  * You can skip the web link if it's empty (null).
  * Your posts won't be saved to a file yet, so they'll be forgotten between runs. 
@@ -36,7 +40,7 @@ import java.util.Scanner;
 public class SocialMediaApp {
     public static void main(String[] args)
     {
-        //create arrayss
+        //create arrays
         Post[] posts = new Post[5];
         User[] users = new User[3];
 
@@ -51,22 +55,31 @@ public class SocialMediaApp {
         posts[2] = new Post("User3","Jan 19, 2016 7:37PM", "Keep posting", "123.com");
         posts[3] = new Post("User1", "Jan 19, 2016 7:38PM", "Do I need a web address?");
         posts[4] = new Post("User2","Jan 19, 2016 7:39PM", "Web addresses are optional :-)", "abc.com");
-
-        //Display Welcome
-        System.out.println("Welcome to the Social Media Posting Site\n");
-
+        
+        displayMenu(users, posts);
+    }
+    public static void displayPosts(Post[] posts)
+    {
         System.out.println("These are the current posts:");
         // Display Posts
-        Post. displayPosts(posts);
-
+        Post.displayPosts(posts);
+    }
+    public static void displayWelcome()
+    {
+        //Display Welcome
+        System.out.println("Welcome to the Social Media Posting Site\n");
+    }
+    public static void displayUsers(User[] users)
+    {
         System.out.println("\nThese are the current users::");
         // Display Users
         User.displayUsers(users);
-
+    }
+    public static void displayMenu(User[] users, Post[] posts)
+    {
         String choice = "1";
         Scanner keyboard = new Scanner(System.in);
-
-        //display Menu
+         //display Menu
         do{
 
             System.out.println("        Main Menu");
@@ -79,7 +92,50 @@ public class SocialMediaApp {
             System.out.println("You are currently user \"pug\". What would you like to do?");
             
             choice = keyboard.nextLine();
+            
+            if (choice.equals( "1"))
+            {
+                createUser();
+            }
+            else if (choice.equals( "2"))
+            {
+                becomeUser();
+            }
+            else if (choice.equals( "3"))
+            {
+                createPost();
+            }
+            else if (choice.equals( "4"))
+            {
+                System.out.println("Printing All Posts"); 
+                displayPosts(posts);
+            }
+            else if (choice.equals( "5"))
+            {
+                System.out.println("Printing all users"); 
+                displayUsers(users);
+            }
+            else if (choice.equals( "6"))
+            {
+                System.out.println("Thank you.");
+            }
+            else
+            {
+                System.out.println("Invalid choice. Enter 1 - 6: "); 
+            }
           
         }while(!(choice.equals( "6")));
+    }
+    public static void createUser()
+    {
+        System.out.println("Creating a new user");
+    }
+    public static void becomeUser()
+    {
+         System.out.println("Becoming an existing user"); 
+    }
+    public static void createPost()
+    {
+        System.out.println("Creating a post as a current user"); 
     }
 }
